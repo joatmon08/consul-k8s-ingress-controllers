b40bf074aa56d34fe10443c088f47e1a36172d50
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-06-04 14:37:06 -0400

    Add intentions configuration

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ helm repo add kong https://charts.konghq.com
 helm repo update
 
 # kubectl create namespace kong
-helm install -n kong example kong/kong -f kong/values.yaml
-# helm install -n default example kong/kong -f kong/values.yaml
+# helm install -n kong example kong/kong -f kong/values.yaml
+helm install -n default example kong/kong -f kong/values.yaml
 kubectl apply -f kong/kubernetes/
 
 HOST=$(kubectl get svc --namespace kong consul-kong-proxy -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
@@ -24,4 +24,9 @@ export PROXY_IP=${HOST}:${PORT}
 curl $PROXY_IP
 
 
+# traefik
 
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo update
+
+helm install -n default traefik traefik/traefik -f traefik/values.yaml
